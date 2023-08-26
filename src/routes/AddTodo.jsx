affff9aaa613e6e28e38f00d792ace50e030b21d
Hassan Ali <hassanali14563@gmail.com>
2023-08-26 08:10:19 +0500

    added and designed form

--- src/routes/AddTodo.jsx
+++ src/routes/AddTodo.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const AddTodo = () => {
-  return <div>AddTodo</div>;
+  return (
+    <>
+      <form className="form">
+        <h4>add new todo</h4>
+        <div className="formRow">
+          <div className="formField">
+            <input type="text" placeholder="Enter Todo" />
+          </div>
+          <div className="formField">
+            <input type="date" placeholder="Select Date" />
+          </div>
+        </div>
+        <div className="formCol">
+          <textarea rows="4" placeholder="Description"></textarea>
+        </div>
+        <div className="formCol">
+          <textarea rows="2" placeholder="Additional Info"></textarea>
+        </div>
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
+  );
 };
 
 export default AddTodo;
